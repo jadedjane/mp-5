4fefc452b9d179fb1c1799a4c4a858d19d4007b7
Jenny Yang <jennyang@bu.edu>
2026-04-10 16:12:21 -0400

    Fix MongoDB client connection for Vercel

--- lib/db.ts
+++ lib/db.ts
@@ -1,30 +1,39 @@
-import {Collection, Db, MongoClient} from "mongodb";
+import { Collection, Db, MongoClient } from "mongodb";
 
-const MONGO_URI = process.env.MONGO_URI as string;
+const MONGO_URI = process.env.MONGO_URI;
 
-if(!MONGO_URI){
-    throw new Error("Something is wrong with your key");
+if (!MONGO_URI) {
+    throw new Error("MONGO_URI is not defined");
 }
 
 const DB_NAME = "cs-391-mp5";
-
 export const LINKS_COLLECTION = "links-collection";
 
-let client: MongoClient | null=null;
-let db: Db | null=null;
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
-async function connect(): Promise<Db> {
-    if (!client) {
-        client = new MongoClient(MONGO_URI);
-        await client.connect();
-    }
-    return client.db(DB_NAME);
+declare global {
+    // eslint-disable-next-line no-var
+    var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
+if (process.env.NODE_ENV === "development") {
+    if (!global._mongoClientPromise) {
+        client = new MongoClient(MONGO_URI);
+        global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise;
+} else {
+    client = new MongoClient(MONGO_URI);
+    clientPromise = client.connect();
+}
+
+async function getDb(): Promise<Db> {
+    const connectedClient = await clientPromise;
+    return connectedClient.db(DB_NAME);
+}
 
 export default async function getCollection(collectionName: string): Promise<Collection> {
-    if (!db) {
-        db = await connect();
-    }
+    const db = await getDb();
     return db.collection(collectionName);
 }
