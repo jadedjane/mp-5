9dc1768486f4f2a57c2c2e220fcc3dee1a2039c7
Jenny Yang <jennyang@bu.edu>
2026-04-10 16:29:07 -0400

    changes

--- lib/db.ts
+++ lib/db.ts
@@ -1,39 +1,30 @@
-import { Collection, Db, MongoClient } from "mongodb";
+import {Collection, Db, MongoClient} from "mongodb";
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI as string;
 
-if (!MONGO_URI) {
-    throw new Error("MONGO_URI is not defined");
+if(!MONGO_URI){
+    throw new Error("Something is wrong with your key");
 }
 
 const DB_NAME = "cs-391-mp5";
+
 export const LINKS_COLLECTION = "links-collection";
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
+let client: MongoClient | null=null;
+let db: Db | null=null;
 
-declare global {
-    // eslint-disable-next-line no-var
-    var _mongoClientPromise: Promise<MongoClient> | undefined;
-}
-
-if (process.env.NODE_ENV === "development") {
-    if (!global._mongoClientPromise) {
+async function connect(): Promise<Db> {
+    if (!client) {
         client = new MongoClient(MONGO_URI);
-        global._mongoClientPromise = client.connect();
+        await client.connect();
     }
-    clientPromise = global._mongoClientPromise;
-} else {
-    client = new MongoClient(MONGO_URI);
-    clientPromise = client.connect();
+    return client.db(DB_NAME);
 }
 
-async function getDb(): Promise<Db> {
-    const connectedClient = await clientPromise;
-    return connectedClient.db(DB_NAME);
-}
 
 export default async function getCollection(collectionName: string): Promise<Collection> {
-    const db = await getDb();
+    if (!db) {
+        db = await connect();
+    }
     return db.collection(collectionName);
 }
